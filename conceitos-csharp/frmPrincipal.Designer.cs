15e9036e2aabc033e8b63d6d0eb715d925c372ef
luan.coesposito <luan.coesposito@SMP0711494W11-1>
2026-02-04 21:36:36 -0300

    calculos

--- conceitos-csharp/frmPrincipal.Designer.cs
+++ conceitos-csharp/frmPrincipal.Designer.cs
@@ -28,12 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmPrincipal";
+            groupBox1 = new GroupBox();
+            btnCadastrar = new Button();
+            dtpNasc = new DateTimePicker();
+            txtTelefone = new TextBox();
+            txtCPF = new TextBox();
+            txtSenha = new TextBox();
+            txtEmail = new TextBox();
+            txtNome = new TextBox();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            btnCalcular = new Button();
+            txtVR = new TextBox();
+            txtVT = new TextBox();
+            txtIRRF = new TextBox();
+            txtINSS = new TextBox();
+            txtSalarioBruto = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnCadastrar);
+            groupBox1.Controls.Add(dtpNasc);
+            groupBox1.Controls.Add(txtTelefone);
+            groupBox1.Controls.Add(txtCPF);
+            groupBox1.Controls.Add(txtSenha);
+            groupBox1.Controls.Add(txtEmail);
+            groupBox1.Controls.Add(txtNome);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Century Gothic", 9F);
+            groupBox1.Location = new Point(12, 22);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(207, 313);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Cadastro";
+            // 
+            // btnCadastrar
+            // 
+            btnCadastrar.BackColor = Color.DarkGreen;
+            btnCadastrar.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCadastrar.ForeColor = Color.White;
+            btnCadastrar.Location = new Point(31, 258);
+            btnCadastrar.Name = "btnCadastrar";
+            btnCadastrar.Size = new Size(136, 42);
+            btnCadastrar.TabIndex = 11;
+            btnCadastrar.Text = "Cadastrar";
+            btnCadastrar.UseVisualStyleBackColor = false;
+            btnCadastrar.Click += btnCadastrar_Click;
+            // 
+            // dtpNasc
+            // 
+            dtpNasc.Location = new Point(31, 191);
+            dtpNasc.Name = "dtpNasc";
+            dtpNasc.Size = new Size(136, 22);
+            dtpNasc.TabIndex = 10;
+            // 
+            // txtTelefone
+            // 
+            txtTelefone.Location = new Point(31, 220);
+            txtTelefone.Name = "txtTelefone";
+            txtTelefone.PlaceholderText = "Telefone";
+            txtTelefone.Size = new Size(136, 22);
+            txtTelefone.TabIndex = 5;
+            // 
+            // txtCPF
+            // 
+            txtCPF.Location = new Point(31, 162);
+            txtCPF.Name = "txtCPF";
+            txtCPF.PlaceholderText = "CPF";
+            txtCPF.Size = new Size(136, 22);
+            txtCPF.TabIndex = 6;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(31, 133);
+            txtSenha.Name = "txtSenha";
+            txtSenha.PlaceholderText = "Senha";
+            txtSenha.Size = new Size(136, 22);
+            txtSenha.TabIndex = 7;
+            txtSenha.UseSystemPasswordChar = true;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(31, 104);
+            txtEmail.Name = "txtEmail";
+            txtEmail.PlaceholderText = "Email";
+            txtEmail.Size = new Size(136, 22);
+            txtEmail.TabIndex = 8;
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(31, 75);
+            txtNome.Name = "txtNome";
+            txtNome.PlaceholderText = "Nome";
+            txtNome.Size = new Size(136, 22);
+            txtNome.TabIndex = 9;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(1, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(199, 23);
+            label1.TabIndex = 4;
+            label1.Text = "Cadastro de Usuário";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnCalcular);
+            groupBox2.Controls.Add(txtVR);
+            groupBox2.Controls.Add(txtVT);
+            groupBox2.Controls.Add(txtIRRF);
+            groupBox2.Controls.Add(txtINSS);
+            groupBox2.Controls.Add(txtSalarioBruto);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Font = new Font("Century Gothic", 9F);
+            groupBox2.Location = new Point(225, 22);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(207, 313);
+            groupBox2.TabIndex = 4;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Salário";
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.BackColor = Color.DarkGreen;
+            btnCalcular.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCalcular.ForeColor = Color.White;
+            btnCalcular.Location = new Point(31, 258);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(136, 42);
+            btnCalcular.TabIndex = 11;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = false;
+            // 
+            // txtVR
+            // 
+            txtVR.Location = new Point(31, 177);
+            txtVR.Name = "txtVR";
+            txtVR.PlaceholderText = "VR";
+            txtVR.Size = new Size(136, 22);
+            txtVR.TabIndex = 5;
+            // 
+            // txtVT
+            // 
+            txtVT.Location = new Point(31, 148);
+            txtVT.Name = "txtVT";
+            txtVT.PlaceholderText = "VT";
+            txtVT.Size = new Size(136, 22);
+            txtVT.TabIndex = 6;
+            // 
+            // txtIRRF
+            // 
+            txtIRRF.Location = new Point(31, 119);
+            txtIRRF.Name = "txtIRRF";
+            txtIRRF.PlaceholderText = "IRRF";
+            txtIRRF.Size = new Size(136, 22);
+            txtIRRF.TabIndex = 7;
+            // 
+            // txtINSS
+            // 
+            txtINSS.Location = new Point(31, 90);
+            txtINSS.Name = "txtINSS";
+            txtINSS.PlaceholderText = "INSS";
+            txtINSS.Size = new Size(136, 22);
+            txtINSS.TabIndex = 8;
+            // 
+            // txtSalarioBruto
+            // 
+            txtSalarioBruto.Location = new Point(31, 61);
+            txtSalarioBruto.Name = "txtSalarioBruto";
+            txtSalarioBruto.PlaceholderText = "Salário Bruto";
+            txtSalarioBruto.Size = new Size(136, 22);
+            txtSalarioBruto.TabIndex = 9;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(31, 216);
+            label3.Name = "label3";
+            label3.Size = new Size(32, 23);
+            label3.TabIndex = 4;
+            label3.Text = "R$";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(10, 31);
+            label2.Name = "label2";
+            label2.Size = new Size(182, 23);
+            label2.TabIndex = 4;
+            label2.Text = "Cálculo de Salário";
+            // 
+            // frmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "frmPrincipal";
+            Text = "frmPrincipal";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private Button btnCadastrar;
+        private DateTimePicker dtpNasc;
+        private TextBox txtTelefone;
+        private TextBox txtCPF;
+        private TextBox txtSenha;
+        private TextBox txtEmail;
+        private TextBox txtNome;
+        private Label label1;
+        private GroupBox groupBox2;
+        private DateTimePicker dateTimePicker1;
+        private TextBox txtVR;
+        private TextBox txtVT;
+        private TextBox txtIRRF;
+        private TextBox txtINSS;
+        private TextBox txtSalarioBruto;
+        private Label label2;
+        private Label label3;
+        private Button btnCalcular;
     }
 }
--- conceitos-csharp/frmPrincipal.cs
+++ conceitos-csharp/frmPrincipal.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace conceitos_csharp
 {
@@ -13,6 +14,123 @@ namespace conceitos_csharp
         public frmPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Nome: {txtNome.Text}\n" +
+                $"Email: {txtEmail.Text}\n" +
+                $"Senha: {txtSenha.Text}\n" +
+                $"CPF: {txtCPF.Text}\n" +
+                $"Data de Nascimento Value: {dtpNasc.Value}\n" +
+                $"Data de Nascimento Value: {dtpNasc.Text}\n" +
+                $"Telefone: {txtTelefone.Text}");
+        }
+
+
+
+        private void CalcularINSS()
+        {
+
+        }
+
+
+
+
+
+
+
+        // Calcula INSS usando faixas progressivas (valores exemplificativos, ajustar conforme tabela vigente)
+        private decimal CalculateINSS(decimal salario)
+        {
+            // Exemplo de faixas (valores e alíquotas fictícias para demonstração):
+            // até 1212.00 => 7.5%
+            // 1212.01 até 2427.35 => 9%
+            // 2427.36 até 3641.03 => 12%
+            // 3641.04 até 7087.22 => 14%
+
+            decimal desconto = 0m;
+
+            var restante = salario;
+
+            // Faixa 1
+            var faixa1 = 1212.00m;
+            if (restante > 0)
+            {
+                var valor = Math.Min(restante, faixa1);
+                desconto += valor * 0.075m;
+                restante -= valor;
+            }
+
+            // Faixa 2
+            var faixa2 = 2427.35m - 1212.00m;
+            if (restante > 0)
+            {
+                var valor = Math.Min(restante, faixa2);
+                desconto += valor * 0.09m;
+                restante -= valor;
+            }
+
+            // Faixa 3
+            var faixa3 = 3641.03m - 2427.35m;
+            if (restante > 0)
+            {
+                var valor = Math.Min(restante, faixa3);
+                desconto += valor * 0.12m;
+                restante -= valor;
+            }
+
+            // Faixa 4
+            var faixa4 = 7087.22m - 3641.03m;
+            if (restante > 0)
+            {
+                var valor = Math.Min(restante, faixa4);
+                desconto += valor * 0.14m;
+                restante -= valor;
+            }
+
+            // Se salário acima do teto, desconto máximo já calculado até o teto (7087.22)
+
+            return Math.Round(desconto, 2);
+        }
+
+        // Calcula IRRF com base na base de cálculo (salário - INSS)
+        private decimal CalculateIRRF(decimal baseCalculo)
+        {
+            // Tabela exemplificativa (alíquotas e parcela a deduzir) - ajustar conforme tabela vigente:
+            // até 1903.98 => isento
+            // 1903.99 até 2826.65 => 7.5% - parcela 142.80
+            // 2826.66 até 3751.05 => 15% - parcela 354.80
+            // 3751.06 até 4664.68 => 22.5% - parcela 636.13
+            // acima 4664.68 => 27.5% - parcela 869.36
+
+            decimal aliquota = 0m;
+            decimal parcela = 0m;
+
+            if (baseCalculo <= 1903.98m)
+            {
+                return 0m;
+            }
+            else if (baseCalculo <= 2826.65m)
+            {
+                aliquota = 0.075m; parcela = 142.80m;
+            }
+            else if (baseCalculo <= 3751.05m)
+            {
+                aliquota = 0.15m; parcela = 354.80m;
+            }
+            else if (baseCalculo <= 4664.68m)
+            {
+                aliquota = 0.225m; parcela = 636.13m;
+            }
+            else
+            {
+                aliquota = 0.275m; parcela = 869.36m;
+            }
+
+            var irrf = (baseCalculo * aliquota) - parcela;
+            if (irrf < 0) irrf = 0m;
+            return Math.Round(irrf, 2);
         }
     }
 }
